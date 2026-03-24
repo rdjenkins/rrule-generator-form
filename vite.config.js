6bc6b775515bad37d7d785d7b892708dcfd9e3f7
Dean Jenkins <richarddeanjenkins@gmail.com>
2026-03-24 23:12:04 +0000

    Fix script source path in index.html and set base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
+    base: '',
     plugins: [
         cssInjectedByJsPlugin(),
     ],
